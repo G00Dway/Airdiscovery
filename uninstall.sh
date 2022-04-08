26bc5c2d926133d30fb885904a100557d7425492
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2022-04-08 11:56:55 +0000

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 echo "[*] Uninstalling Airdiscovery..."
-rm -rf /usr/share/airdiscovery
-rm -rf /usr/bin/airdiscovery
+rm -rf /usr/share/airdiscover
+rm -rf /usr/bin/airdiscover
 rm -rf /root/.airdr
 sleep 1
 echo "[+] Done."
